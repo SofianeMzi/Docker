6d9f1c3e71da88b0c8222a0a40431e917d0d3383
Sofiane <sofiane.meziti@gmail.com>
2020-05-07 16:10:57 +0200

    script qui permet le redémarrage du conteneur et de l'application qui tournait dans le conteneur, option de comptage de redémarrage, de temps de redémarrage du conteneur et de l'application

---
+++ nodetest/restart-pingpong.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Code permettant de lancer un conteneur, lancer l'application pingpong, attendre le crash du conteneur, relancer le conteneur et l'application automatiquement. Ce code contient un compteur de redémarrage ainsi que le temps de redémarrage du SERVICE (= de l'application pingpong)
+
+for image_id in $(docker images --filter=reference=imagenodetest --format "{{.ID}}") #supression de l'image précédente
+	do docker rmi -f ${image_id}
+done
+
+docker build -t imagenodetest . # création de la nouvelle image imagenodetest à partir du dockerfile 
+
+for container_id in $(docker ps -aqf "name=nodetest") #supression du conteneur précédent pour éviter un conflit avec le nouveau conteneur qui va être créé 
+		do docker rm -f ${container_id}
+done
+#lancement du conteneur et de l'application pingpong 
+docker run -dt --name nodetest --network mynet -h nodetest imagenodetest ./pingpongR multi
+
+echo "container running, waiting for exit code"
+#attente que le conteneur soit tué 
+i=0 #pour le compteur de redémarrage
+while true
+do
+	((i+=1)) #incrémentation du compteur
+#j'attend que la fonction wait détecte la fermeture du conteneur
+	cmd_output=$(docker wait nodetest) 
+#j'attend que la fotncion wait() détecte un changement d'état du process créé par "docker wait nodetest"
+	wait $!  
+	echo "container stopped, exit code: $cmd_output"
+	echo "container & application restart n°$i"
+	
+#restart du conteneur en utilisant "docker start"
+	start=`date +%s%N | cut -b1-13` #mesure du temps
+	docker start nodetest
+	end=`date +%s%N | cut -b1-13` #mesure du temps
+	runtime=$((end-start))
+	echo "container & application restarted in $runtime ms"
+
+done
+
